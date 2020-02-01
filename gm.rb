1ac97c83d1544310bd75a82cb6f302a122b5a0e7
decent-array-5141 <lfzernin@gmail.com>
2020-02-01 20:13:26 +0000

    Automatically backed up by Learn

--- gm.rb
+++ gm.rb
@@ -1,12 +1,9 @@
+puts 'Hi Grandma!'
+command = gets.chomp
 
-command = ''
-
-while command = ''
-  if command != 'command.downcase'
+while command != 'command.downcase'
   puts 'HUH?! SPEAK UP, SON!'
   command = gets.chomp
-else
-  if command == 'command.downcase'
-    puts 'NOT SINCE 2020!'
-  end
-  
+end
+
+puts 'NOT SINCE 2020!'
