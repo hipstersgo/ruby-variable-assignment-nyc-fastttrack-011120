31ccbce5b282bd599a57851aeb28dddf5685b7bf
decent-array-5141 <lfzernin@gmail.com>
2020-02-01 19:42:03 +0000

    Automatically backed up by Learn

--- gm.rb
+++ gm.rb
@@ -3,13 +3,12 @@
 command = ''
 
 puts hello grandma
+command = gets.chomp
 
-if command != 'command.upcase'
+if command =! 'command.lowercase'
   puts 'HUH?! SPEAK UP, SON!
-  command = gets.chomp
-
-
-while command == 'command.upcase'
+else
+  if command == 'command.upcase'
   puts 'NO, NOT SINCE 1938!!'
   command = gets.chomp
 end
