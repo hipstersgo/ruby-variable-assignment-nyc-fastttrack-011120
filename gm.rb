f08d4f99e1028e6155addaacf10f779355457577
decent-array-5141 <lfzernin@gmail.com>
2020-02-01 20:35:10 +0000

    Automatically backed up by Learn

--- gm.rb
+++ gm.rb
@@ -3,9 +3,10 @@ command = gets.chomp
 
 while command == command.downcase
   puts 'HUH?! SPEAK UP, SON!'
+if command != command.downcase
+    puts 'NOT SINCE ' + rand(2000).to_s
   command = gets.chomp
 
-if command != command.downcase
-  puts 'NOT SINCE ' + rand(2000).to_s
+
   command = gets.chomp
 end
