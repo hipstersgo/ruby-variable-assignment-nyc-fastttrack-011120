88065b13743c772c0d7ea20121f2ff3da306c56b
decent-array-5141 <lfzernin@gmail.com>
2020-02-01 17:55:03 +0000

    Automatically backed up by Learn

--- number.rb
+++ number.rb
@@ -1,7 +1,7 @@
 puts "Hello, what's your favorite number?"
 number = gets.chomp
 puts number + ' is a fine number! '
-var1 = number.to_i
+var1 = number
 var2 = 1
-var3 = var1 + var2
-puts 'Ah, but' + var3 + 'is a much better number'
+puts var1 + var2.to_i
+ 
