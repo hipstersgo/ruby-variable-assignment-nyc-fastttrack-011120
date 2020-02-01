2ad7b20e2a273e75d4c411ab27171c799624aaae
decent-array-5141 <lfzernin@gmail.com>
2020-02-01 19:00:01 +0000

    Automatically backed up by Learn

--- gm.rb
+++ gm.rb
@@ -1,9 +1,10 @@
 puts 'hey grandma!'
 
 command = ''
+command = gets.chomp
+
 while command != 'command.capitalize'
   puts 'HUH?! SPEAK UP, SONNY!'
 else
   puts 'NO, NOT SINCE 1938!'
-  command = gets.chomp
 end
